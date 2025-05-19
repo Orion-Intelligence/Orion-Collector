3b63e1c26134dd34f549db842d5fc0869f379182
Usman Ali <168332899+usmancout@users.noreply.github.com>
2025-05-20 00:06:40 +0500

    Update _imncrewwfkbjkhr2oylerfm5qtbzfphhmpcfag43xc2kfgvluqtlgoid.py

--- leak_collector/scripts/_imncrewwfkbjkhr2oylerfm5qtbzfphhmpcfag43xc2kfgvluqtlgoid.py
+++ leak_collector/scripts/_imncrewwfkbjkhr2oylerfm5qtbzfphhmpcfag43xc2kfgvluqtlgoid.py
@@ -8,8 +8,7 @@ from crawler.crawler_instance.local_shared_model.rule_model import RuleModel, Fe
 from crawler.crawler_services.redis_manager.redis_controller import redis_controller
 from crawler.crawler_services.redis_manager.redis_enums import REDIS_COMMANDS, CUSTOM_SCRIPT_REDIS_KEYS
 from crawler.crawler_services.shared.helper_method import helper_method
-
-
+from bs4 import BeautifulSoup
 class _imncrewwfkbjkhr2oylerfm5qtbzfphhmpcfag43xc2kfgvluqtlgoid(leak_extractor_interface, ABC):
     _instance = None
 
@@ -68,25 +67,56 @@ class _imncrewwfkbjkhr2oylerfm5qtbzfphhmpcfag43xc2kfgvluqtlgoid(leak_extractor_i
             self.callback()
 
     def parse_leak_data(self, page: Page):
-        
-        m_content = ""
 
-        card_data = leak_model(
-            m_title=page.title(),
-            m_url=page.url,
-            m_base_url=self.base_url,
-            m_screenshot="",
-            m_content=m_content,
-            m_network=helper_method.get_network_type(self.base_url),
-            m_important_content=m_content,
-            m_weblink=[],
-            m_dumplink=[],
-            m_content_type=["leaks"],
-        )
+        page.wait_for_selector("div.block")
+        html = page.content()
+        soup = BeautifulSoup(html, "html.parser")
 
-        entity_data = entity_model(
-            m_email_addresses=helper_method.extract_emails(m_content),
-            m_phone_numbers=helper_method.extract_phone_numbers(m_content),
-        )
+        card_blocks = soup.find_all("div", class_="block")
 
-        self.append_leak_data(card_data, entity_data)
+        for card in card_blocks:
+            title_tag = card.find("h3", class_="yellow-text")
+            m_title = title_tag.get_text(strip=True) if title_tag else ""
+
+            desc_tag = card.find("p")
+            m_content = desc_tag.get_text(strip=True) if desc_tag else ""
+
+            words = m_content.split()
+            if len(words) > 500:
+                important_content = ' '.join(words[:500])
+            else:
+                important_content = m_content
+
+            download_link = ""
+            spoiler_content = card.find("div", class_="spoiler-content")
+            if spoiler_content:
+                a_tag = spoiler_content.find("a", href=True)
+                if a_tag:
+                    download_link = a_tag["href"]
+
+            image_urls = []
+            images_div = card.find("div", class_="images")
+            if images_div:
+                for img_tag in images_div.find_all("img", src=True):
+                    image_urls.append(img_tag["src"])
+
+            card_data = leak_model(
+                m_title=m_title,
+                m_url=page.url,
+                m_base_url=self.base_url,
+                m_screenshot=helper_method.get_screenshot_base64(page,m_title,self.base_url),
+                m_content=m_content,
+                m_network=helper_method.get_network_type(self.base_url),
+                m_important_content=important_content,
+                m_logo_or_images=image_urls,
+                m_dumplink=[download_link] if download_link else [],
+                m_content_type=["leaks"],
+
+            )
+
+            entity_data = entity_model(
+                m_email_addresses=helper_method.extract_emails(m_content),
+                m_phone_numbers=helper_method.extract_phone_numbers(m_content),
+            )
+
+            self.append_leak_data(card_data, entity_data)
